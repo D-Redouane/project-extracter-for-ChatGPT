4a5dfcdbba6e3901eab12994dcd961db1d360daf
Redouane DADDIOUAMER <94105088+D-Redouane@users.noreply.github.com>
2024-01-13 13:18:00 +0100

    Add files via upload

---
+++ extracter.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+  
+const sourceFolder = './lab6/src'; // Change this to the path of your source folder
+const outputFile = './output.txt'; // Change this to the desired output file path
+const extensions = ['.jsx', '.css']; // files that end with extentions that you want
+
+function concatenateFiles(folderPath, outputFilePath, depth = 0) {
+  const files = fs.readdirSync(folderPath);
+ 
+  let fileContent = '';
+  let folderContent = '';
+  let subfolderContent = '';
+
+  files.forEach(file => {
+    const filePath = path.join(folderPath, file);
+
+    if (fs.statSync(filePath).isDirectory()) {
+      subfolderContent = concatenateFiles(filePath, outputFilePath, depth + 1);
+
+      if (subfolderContent && subfolderContent.trim()) {
+        folderContent += `\n\n\n\n\n\n\n\n// ${'  '.repeat(depth)}${file}\n${subfolderContent}\n\n`;
+      } else {
+        folderContent += subfolderContent;
+      }
+    } else {
+      for (const extension of extensions) {
+        if (file.endsWith(extension)) {
+          const fileContentToAdd = fs.readFileSync(filePath, 'utf-8');
+          fileContent += `\n\n\n\n\n\n\n\n// ${path.join(folderPath, file)}\n${fileContentToAdd}`;
+          break;
+        }
+      }
+    }
+  });
+
+  const content = folderContent + '\n\n' + fileContent;
+  fs.appendFileSync(outputFilePath, content, 'utf-8');
+}
+
+concatenateFiles(sourceFolder, outputFile);
+console.log('Files concatenated successfully!');
